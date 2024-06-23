167893cc33396b45f9a49b8da3730fd500e77175
Chethan <Chethanprakash720@gmail.com>
2024-06-23 20:29:52 +0530

    basic setup

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from config import TOKEN
 
-# Configure logging
+# logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Define command handlers
+# command handlers
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the Stock Bot! Use /help to see available commands.")
 
@@ -21,24 +21,21 @@ def help_command(update: Update, context: CallbackContext):
     """
     context.bot.send_message(chat_id=update.effective_chat.id, text=help_text)
 
-# Main function to start the bot
+# Start Bot
 def main():
-    # Create an Updater object with the bot's token
     updater = Updater(TOKEN, use_context=True)
 
-    # Get the dispatcher to register handlers
+    
     dp = updater.dispatcher
 
-    # Add command handlers to the dispatcher
+   
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help_command))
 
-    # Start polling for updates from Telegram
+   
     updater.start_polling()
-
-    # Run the bot until you press Ctrl+C
     updater.idle()
 
-# Entry point for the script
+# run
 if __name__ == '__main__':
     main()
