1d17332932117e576e8e0de41b1fb79a96f72914
Chethan <Chethanprakash720@gmail.com>
2024-07-04 17:56:53 +0530

    updated start message

--- handlers/start.py
+++ handlers/start.py
@@ -9,11 +9,11 @@ def start(update: Update, context: CallbackContext) -> None:
         "/price <coin> - Get the latest price of a cryptocurrency\n"
         "/alert <coin> <price> - Set a price alert for a cryptocurrency\n"
         "/chart <coin> - Get a price chart for the last 7 days\n"
-        "/historical <coin> <days> - Get historical price data\n"
+        "/historical <coin> - Get historical price data of past 7 days\n"
         "Example usage:\n"
         "/price bitcoin\n"
         "/alert ethereum 2000\n"
         "/chart dogecoin\n"
-        "/historical cardano 30\n"
+        "/historical cardano \n"
     )
     update.message.reply_text(welcome_message)
